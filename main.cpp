84d61a08fa7d8d9cabcca1ac6667dc8190f52a72
Connor <38581011+3108cb@users.noreply.github.com>
2021-08-25 16:00:54 -0700

    Updated Main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+// Connor Beach Ethan Tran
+//Description: Hello World in C++
+//=======================================================
 
+#include <iostream>
+using namespace std;
+int hello!Function(string name!);
+int hello2Function(string name2);
+int main(){
+  string name1="your name1";
+  hello1Function(name1);
+  string name2="your name2";
+  hello2Function(name2);
+  return 0;
+}
